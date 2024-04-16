300e73dd484395577818eddf3bc028df0248efa7
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2024-04-16 18:00:11 +0545

    yy

--- function.js
+++ function.js
@@ -1,8 +1,14 @@
-function ab(n) {
-  if (n > 5) {
-    return;
-  }
-  console.log(n);
-  ab(n + 1);
+// function ab(n) {
+//   if (n > 5) {
+//     return;
+//   }
+//   console.log(n);
+//   ab(n + 1);
+// }
+// ab(1);
+
+function myFunction(x, y = 10) {
+  console.log(x + y);
+  return x + y;
 }
-ab(1);
+myFunction(5);
