7a17ecb49fe03a268f1f85bea08ba4e27f055e70
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2024-04-17 17:22:44 +0545

    ss

--- function.js
+++ function.js
@@ -1,14 +1,39 @@
-// function ab(n) {
-//   if (n > 5) {
-//     return;
-//   }
-//   console.log(n);
-//   ab(n + 1);
-// }
-// ab(1);
+// print number 1 to 5
+
+function ab(n) {
+  if (n > 5) {
+    return;
+  }
+  console.log(n);
+  ab(n + 1);
+}
+ab(1);
+
+// sum of x and y
 
 function myFunction(x, y = 10) {
   console.log(x + y);
   return x + y;
 }
 myFunction(5);
+
+// print hi 5 time
+
+function show(n) {
+  console.log("Hi", n);
+  if (n >= 5) return;
+  show(n + 1);
+}
+show(1);
+
+//factorial
+
+function factorial(n) {
+  if (n == 1 || n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(4));
+// sum(4);
